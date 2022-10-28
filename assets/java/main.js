2f5fbe4061926605022a60fdfe688990d2303c29
Adithyasankar74 <112875396+Adithyasankar74@users.noreply.github.com>
2022-10-28 21:20:18 +0530

    Still more to work on

---
+++ assets/java/main.js
@@ -0,0 +1,90 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
+
+/*=============== CALCULATE JS ===============*/
+
+const calculateCm = document.getElementById('calculate-cm'),
+      calculateForm = document.getElementById('calculate-form'),
+      calculateKg = document.getElementById('calculate-kg'),
+      calculateMessage = document.getElementById('calculate-message')
+
+const calculateBmi = (e) =>{
+    e.preventDefault()
+
+    //check if fields have value
+    if(calculateCm.value ==='' || calculateKg.value ==='' ){
+        //Add and remove color
+        calculateMessage.classList.remove('color-green')
+        calculateMessage.classList.add('color-red')
+
+        //show message
+
+        calculateMessage.textContent = 'Fill in the Height and Weight'
+
+        //Remove message  three seconds
+
+        setTimeout(() =>{
+            calculateMessage.textContent = ''
+        },3000)
+    } else{
+        const cm = calculateCm.value / 100,
+              kg = calculateKg.value,
+              bmi = Math.round(kg / (cm * cm))
+              
+              
+              
+    }
+}
+.calculateForm.addEventListener('submit', calculateBmi)
+
+
+/*=============== EMAIL JS ===============*/
